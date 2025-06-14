36240ceb93f19107704fb527cd60a6900c877558
Angel D. Lopez <alopez7@me.com>
2025-06-14 18:39:49 -0400

    Added initial project code

---
+++ MortgageCalculator.py
@@ -0,0 +1,16 @@
+def loan_calculator(rate, org_prin, term):
+    monthly_rate = (rate/12)/(100) #Converts mortgage interest rate into a monthly decimal 
+    months = term  * 12 # Converts the choosen annual term dates into months
+    balance = org_prin 
+    monthly_payment = balance * monthly_rate/(1 - (1 + (monthly_rate))**(-(term * 12)))
+    schedule = []
+     
+    for month in range(months):
+        
+        interest = balance * monthly_rate #Calculates monthly interest 
+        principal = monthly_payment - interest #Calculates minthly principal
+        balance -= principal #Calculates end of month mortgage balance
+        schedule.append({"Month" : month + 1, "Principal Payment": principal, "Interest": interest, "Remaining Balance": balance}) #Adds a dictionary inside of a list 
+    return schedule 
+    
+print(loan_calculator(6, 100000, 30))
