d4d1e55f6c58f7b4b0c70c07f92bb1e3e40302a9
Angel D. Lopez <alopez7@me.com>
2025-06-15 10:40:18 -0400

    Edited code - added user input

--- MortgageCalculator.py
+++ MortgageCalculator.py
@@ -6,11 +6,10 @@ def loan_calculator(rate, org_prin, term):
     schedule = []
      
     for month in range(months):
-        
         interest = balance * monthly_rate #Calculates monthly interest 
         principal = monthly_payment - interest #Calculates minthly principal
         balance -= principal #Calculates end of month mortgage balance
         schedule.append({"Month" : month + 1, "Principal Payment": principal, "Interest": interest, "Remaining Balance": balance}) #Adds a dictionary inside of a list 
     return schedule 
     
-print(loan_calculator(6, 100000, 30))
+print(loan_calculator(int(input("What is the interest rate?")), int(input("What is the loan amount?")), int(input("What is the term?"))))
