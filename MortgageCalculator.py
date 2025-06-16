a04a94cd612898d3103569a0bd1fa37f7cde22c3
Angel D. Lopez <alopez7@me.com>
2025-06-16 13:12:25 -0400

    Updated app and README.md

--- MortgageCalculator.py
+++ MortgageCalculator.py
@@ -1,3 +1,6 @@
+import csv
+from tabulate import tabulate
+
 def loan_calculator(rate, org_prin, term):
     monthly_rate = (rate/12)/(100) #Converts mortgage interest rate into a monthly decimal 
     months = term  * 12 # Converts the choosen annual term dates into months
@@ -8,8 +11,20 @@ def loan_calculator(rate, org_prin, term):
     for month in range(months):
         interest = balance * monthly_rate #Calculates monthly interest 
         principal = monthly_payment - interest #Calculates minthly principal
-        balance -= principal #Calculates end of month mortgage balance
-        schedule.append({"Month" : month + 1, "Principal Payment": principal, "Interest": interest, "Remaining Balance": balance}) #Adds a dictionary inside of a list 
-    return schedule 
-    
-print(loan_calculator(int(input("What is the interest rate?")), int(input("What is the loan amount?")), int(input("What is the term?"))))
+        balance -= round(principal, 2) #Calculates end of month mortgage balance and rounds to the nearest hundredth
+        schedule.append({"Month" : month + 1, "Principal Payment": principal, "Interest": interest, "Remaining Balance": balance}) #Adds a dictionary inside of a list
+        amort_list = schedule #stores the dictionary
+
+    return amort_list
+
+def amort_table(list):
+
+    with open("amort_table.csv", "w", newline="") as amortization:
+        writer = csv.DictWriter(amortization, fieldnames=["Month", "Principal Payment", "Interest", "Remaining Balance"])
+        writer.writeheader()
+        writer.writerows(list)
+
+    return list
+
+ask_user = loan_calculator(float(input("What is the interest rate?")), float(input("What is the loan amount?")), int(input("What is the term?")))
+print(tabulate((ask_user), headers="keys", tablefmt="fancy_grid"))
--- README.md
+++ README.md
@@ -11,13 +11,21 @@ This is my first python project - a mortgage calculator.
 
 ##  Overview 
 
-Standard mortgage calculator. Provides monthly payment amount given loan amount, interest rate and term. Amortization table is also provided to illustrate portion of principal vs. portion of interset over time. 
+Standard mortgage calculator. Provides monthly payment amount given loan amount, interest rate and term. Amortization table is also provided to illustrate portion of principal vs. portion of interest over time. 
 
 ## Features 
 -Calculates monthly loan payments
 -Generates an amortization schedule 
 -Print Summary stats to console
 
-## Installation 
+## Roadmap/To-Do
+-Extra payment calculator
+-Ability to sum total principal vs. total interest given extra payment
+-Comparison between different loan options (e.g. 15 years vs. 30 years) 
+
+## Author
+Angel Lopez - aspiring financial software developer. 
+
+
 
 
