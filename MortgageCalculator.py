3c3ce89fe2397bde1bf3c23557425efbdd4e2c55
Angel D. Lopez <alopez7@me.com>
2025-06-19 20:42:41 -0400

    Add methods for class modules

--- MortgageCalculator.py
+++ MortgageCalculator.py
@@ -1,54 +1,47 @@
 import csv
 from tabulate import tabulate
 
-intro_response = input("Hi! Welcome to Simple Mortgage Calculator. Would you like to calculate your monthly mortgage payment? Yes or No\n")
+# LoanCalculator Project
+# What it does: Calculates mortgage monthly payments, interest, principal, and builds amortization schedule
+# Still to do: finish generate_schedule, display_table, export_to_csv
 
-def loan_calculator(org_princinple, rate, term):
-    schedule = []
+def collect_userinput(): #Collets user information which will be used in the class LoanCalculator
+    ask_loan = float(input("What is the loan amount?\n"))
+    ask_rate = float(input("What is the interest rate\n"))
+    ask_term = int(input("What is the term?\n"))
+    return ask_loan, ask_rate, ask_term
 
-    if rate == 0:
-        monthly_rate = 0
-        months = term * 12
-        balance = org_princinple
-        monthly_payment = balance/(term*12)
-    else:
-        monthly_rate = (rate / 12) / (100)  # Converts mortgage interest rate into a monthly decimal
-        months = term * 12  # Converts the choosen annual term dates into months
-        balance = org_princinple
-        monthly_payment = balance * monthly_rate / (1 - (1 + (monthly_rate)) ** (-(term * 12)))
+class LoanCalculator: #Calculates mortgage monthly payments, interest, principal, and builds amortization schedule
 
-    for month in range(months):
-        interest = balance * monthly_rate #Calculates monthly interest 
-        principal = monthly_payment - interest #Calculates minthly principal
-        balance -= round(principal, 2) #Calculates end of month mortgage balance and rounds to the nearest hundredth
-        schedule.append({"Month" : month + 1, "Monthly Payment": monthly_payment, "Principal Payment": principal, "Interest": interest, "Remaining Balance": balance}) #Adds a dictionary inside of a list
+    def __init__(self, original_principle, rate, term):
+        self.original_principle = original_principle
+        self.rate = rate
+        self.term = term
+        self.balance = original_principle
+        self.monthly_rate = (self.rate/12)/(100)
+        self.months = self.term * 12
 
 
-    if schedule[-1]["Remaining Balance"] <= 100:
-        schedule[-1]["Remaining Balance"] = 0
-    else:
+    def monthly_payment(self): #Calculates monthly payment
+        self.monthly_amount = round(self.balance * self.monthly_rate / (1 - ((1 + (self.monthly_rate)) ** -(self.term * 12))), 2) #Equation to calculate monthly payment
+        return f'Your monthly payment is {self.monthly_amount}'
+
+    def interest_payment(self): #Returns the monthly interest payment
+        return self.balance * self.monthly_rate
+        #return f'Your interest payment is {self.interest_amount}'
+
+    def principal_payment(self): #Returns monthly principal payment
+        return self.monthly_amount - self.interest_payment() #Equation to calculate monthly principal payment
+
+    def generate_schedule(self):
         pass
 
-    return schedule
+    def display_table(schedule):
+        pass
 
-def amort_table(list):
+    def export_to_csv(self):
+        pass
 
-    with open("amortizationtable.csv", "w", newline="") as amortization:
-        writer = csv.DictWriter(amortization, fieldnames=["Month", "Monthly Payment", "Principal Payment", "Interest", "Remaining Balance"])
-        writer.writeheader()
-        writer.writerows(list)
-
-    return list
-
-loan_amount = input("What is the loan amount?")
-interest_rate = input("What is the interest rate?")
-term_length = input("What is the term?")
-
-if intro_response.lower() == "yes":
-    ask_user = loan_calculator(float(loan_amount), float(interest_rate), int(term_length))
-    print("Here's an amortization table with your monthly payment along with a break down of your monthly principal vs. interest payment\n:", tabulate((ask_user), headers="keys", tablefmt="fancy_grid"))
-
-elif intro_response.lower() == "no":
-    exit()
-
-
+ask_loan, ask_rate, ask_term = collect_userinput()
+test = LoanCalculator(ask_loan, ask_rate, ask_term)
+print(test.monthly_payment(),test.principal_payment(), test.interest_payment())
